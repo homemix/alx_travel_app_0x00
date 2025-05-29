a89f3aa18d0e5d8a40a4694ebb720810ad33aefe
homemix <kenmutati@gmail.com>
2025-05-29 11:32:32 +0300

    ADD: models and seeds for the project database.

---
+++ alx_travel_app/alx_travel_app/listings/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import Property,Booking
+
+class PropertySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Property
+        fields = ['name','description','location','price_per_night']
+
+
+class BookingSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Booking
+        fields = ['start_date','end_date','total_price','status']
